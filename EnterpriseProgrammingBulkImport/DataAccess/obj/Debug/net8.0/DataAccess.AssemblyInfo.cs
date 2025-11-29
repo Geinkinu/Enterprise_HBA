02382205f7e63b6bce2b694c0580564e06025ee7
Gerrard Compton <gerrard.compton06@gmail.com>
2025-11-29 23:54:34 +0100

    Configure Production settings and add FTP deployment setup for myasp.net

--- EnterpriseProgrammingBulkImport/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ EnterpriseProgrammingBulkImport/DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0afdb1099da97f7881810196bb18f6edb7d90ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+522875408418920ed99ab6a6b504af8200902f82")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
